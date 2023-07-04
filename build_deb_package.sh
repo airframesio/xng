5e4673b9b81eb1b5839e0d0a6aa88e337fffe9ef
andermatt64 <andy@airframes.io>
2023-07-04 15:42:46 -0400

    Packaging

---
+++ build_deb_package.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DEB_ARCH=$(dpkg --print-architecture)
+DEB_BUILD_ROOT=$(pwd)/build
+
+mkdir -p ${DEB_BUILD_ROOT}/xng/DEBIAN
+mkdir -p ${DEB_BUILD_ROOT}/xng/usr/bin
+
+cp $(pwd)/target/release/xng ${DEB_BUILD_ROOT}/xng/usr/bin
+sed -e s/CURRENT_ARCH/${DEB_ARCH} packaging/control > ${DEB_BUILD_ROOT}/xng/DEBIAN/control
+
+pushd ${DEB_BUILD_ROOT} && \
+  dpbg-deb --build xng && \
+  popd
