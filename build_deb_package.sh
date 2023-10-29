55b6f6069faf2d0c1bd9b47003bd56dac2e8dc67
andermatt64 <andy@airframes.io>
2023-10-29 19:33:12 -0400

    Update README

--- build_deb_package.sh
+++ build_deb_package.sh
@@ -9,7 +9,7 @@ DEB_PKG_NAME=xng-${DEB_PKG_VERSION}-${DEB_ARCH}
 mkdir -p ${DEB_BUILD_ROOT}/${DEB_PKG_NAME}/DEBIAN
 mkdir -p ${DEB_BUILD_ROOT}/${DEB_PKG_NAME}/usr/bin
 
-cp $(pwd)/target/release/${DEB_PKG_NAME} ${DEB_BUILD_ROOT}/${DEB_PKG_NAME}/usr/bin
+cp $(pwd)/target/release/xng ${DEB_BUILD_ROOT}/${DEB_PKG_NAME}/usr/bin
 sed -e s/CURRENT_ARCH/${DEB_ARCH}/ $(pwd)/packaging/control > ${DEB_BUILD_ROOT}/${DEB_PKG_NAME}/DEBIAN/control
 
 pushd ${DEB_BUILD_ROOT} && \
